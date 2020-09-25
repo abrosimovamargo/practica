687bfe67c3f37d32b74417010df18bd39f48f722
abrosimovamargo <abrosimovamargo@yandex.ru>
2020-09-25 19:06:09 +0300

    Добавлена обработка событий мышки (два метода: мышка вошла и мышка вышла за пределы кнопки)

--- src/main/java/ru/ssau/tk/abrosimovamargo/practice/Puzzle/PuzzleButton.java
+++ src/main/java/ru/ssau/tk/abrosimovamargo/practice/Puzzle/PuzzleButton.java
@@ -1,8 +1,20 @@
 package ru.ssau.tk.abrosimovamargo.practice.Puzzle;
 
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class PuzzleButton extends JButton {
     private void initUI(){
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                super.mouseEntered(e);
+            }
+            @Override
+            public void mouseExited(MouseEvent e) {
+                super.mouseExited(e);
+            }
+        });
     }
 }
