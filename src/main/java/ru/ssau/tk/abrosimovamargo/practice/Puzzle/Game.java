25b981f4468f7abe93335af0c0e5f3dd88dbb76e
abrosimovamargo <abrosimovamargo@yandex.ru>
2020-09-29 15:37:04 +0300

    Создано поле типа BufferedImage для первоначальной картинки

--- src/main/java/ru/ssau/tk/abrosimovamargo/practice/Puzzle/Game.java
+++ src/main/java/ru/ssau/tk/abrosimovamargo/practice/Puzzle/Game.java
@@ -2,10 +2,12 @@ package ru.ssau.tk.abrosimovamargo.practice.Puzzle;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 
 public class Game extends JFrame{
     private ArrayList<Point> solution;
     private JPanel panel;
+    private BufferedImage source;
     }
