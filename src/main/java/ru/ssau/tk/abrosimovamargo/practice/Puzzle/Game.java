db9e845a6b1a0cedb49962a9bd329edd05d2c49a
abrosimovamargo <abrosimovamargo@yandex.ru>
2020-09-29 15:27:05 +0300

    Создано поле отвечающее за проверку правильности собранной картинки

--- src/main/java/ru/ssau/tk/abrosimovamargo/practice/Puzzle/Game.java
+++ src/main/java/ru/ssau/tk/abrosimovamargo/practice/Puzzle/Game.java
@@ -1,7 +1,11 @@
 package ru.ssau.tk.abrosimovamargo.practice.Puzzle;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
 
 public class Game extends JFrame{
+    private ArrayList<Point> solution;
 
 }
