3801252209988bd4b5b351011e3b084f498c9416
abrosimovamargo <abrosimovamargo@yandex.ru>
2020-11-22 19:00:33 +0300

    Задание 1.8 Классу Points добавлены методы scalarProduct() и vectorProduct()

--- src/main/java/ru/ssau/tk/abrosimovamargo/practice/zadachi/Points.java
+++ src/main/java/ru/ssau/tk/abrosimovamargo/practice/zadachi/Points.java
@@ -31,5 +31,14 @@ public class Points {
     public static Point inverse(Point t) {
         return new Point(1 / t.x, 1 / t.y, 1 / t.z);
     }
+    public static double scalarProduct(Point t1, Point t2) {
+        return t1.x * t2.x + t1.y * t2.y + t1.z * t2.z;
+    }
+    public static Point vectorProduct(Point t1, Point t2) {
+        double newX = t1.y * t2.z - t2.y * t1.z;
+        double newY = t1.x * t2.z - t2.x * t1.z;
+        double newZ = t1.x * t2.y - t2.x * t1.y;
+        return new Point(1 * newX, -1 * newY, 1 * newZ);
+    }
 
 }
