a16c0e1edd46e9126950bf942fc2a4fcaefd44d5
abrosimovamargo <abrosimovamargo@yandex.ru>
2020-11-22 18:02:55 +0300

    Задание 1.5 Классу Points добавлен метод enlarge()

--- src/main/java/ru/ssau/tk/abrosimovamargo/practice/zadachi/Points.java
+++ src/main/java/ru/ssau/tk/abrosimovamargo/practice/zadachi/Points.java
@@ -19,4 +19,7 @@ public class Points {
 
         return new Point(t1.x / t2.x, t1.y / t2.y, t1.z / t2.z);
     }
+    public static Point enlarge(Point t, double a) {
+        return new Point(t.x * a, t.y * a, t.z * a);
+    }
 }
