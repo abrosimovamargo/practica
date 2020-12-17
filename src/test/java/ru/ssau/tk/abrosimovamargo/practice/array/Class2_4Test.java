23dd79d4f96bdf90f903a71d683b1cbb40887b06
abrosimovamargo <abrosimovamargo@yandex.ru>
2020-12-18 00:17:36 +0300

    Тест к заданию 2.4

--- src/test/java/ru/ssau/tk/abrosimovamargo/practice/array/Class2_4Test.java
+++ src/test/java/ru/ssau/tk/abrosimovamargo/practice/array/Class2_4Test.java
@@ -5,8 +5,13 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 public class Class2_4Test {
+    int[] array = new Class2_4().Array4(7);
 
     @Test
     public void testArray4() {
+        assertEquals(array.length, 7);
+        assertEquals(array[2], 10);
+        assertEquals(array[3], 8);
     }
 }
+
