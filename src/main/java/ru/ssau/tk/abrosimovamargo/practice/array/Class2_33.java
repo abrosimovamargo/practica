cb90488f641bc9c508cbbc8d9fab8a4ce316d7c4
abrosimovamargo <abrosimovamargo@yandex.ru>
2020-12-22 23:50:59 +0300

    Задание 2.33

---
+++ src/main/java/ru/ssau/tk/abrosimovamargo/practice/array/Class2_33.java
@@ -0,0 +1,9 @@
+package ru.ssau.tk.abrosimovamargo.practice.array;
+
+public class Class2_33 {
+    static void Array33(String[] arr) {
+        for (String i : arr) {
+            System.out.print(i);
+        }
+    }
+}
---
+++ src/test/java/ru/ssau/tk/abrosimovamargo/practice/array/Class2_33Test.java
@@ -0,0 +1,14 @@
+package ru.ssau.tk.abrosimovamargo.practice.array;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class Class2_33Test {
+
+    @Test
+    public void testArray33() {
+        String[] arr = new String[]{"marga", "rita"};
+        Class2_33.Array33(arr);
+    }
+}
