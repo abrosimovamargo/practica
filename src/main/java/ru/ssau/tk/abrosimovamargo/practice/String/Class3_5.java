9fe38d2ee154b06c242e91d9cb5ad50f43963236
abrosimovamargo <abrosimovamargo@yandex.ru>
2020-12-25 21:55:32 +0300

    Задание 3.5

--- src/main/java/ru/ssau/tk/abrosimovamargo/practice/String/Class3_5.java
+++ src/main/java/ru/ssau/tk/abrosimovamargo/practice/String/Class3_5.java
@@ -1,4 +1,7 @@
 package ru.ssau.tk.abrosimovamargo.practice.String;
 
 public class Class3_5 {
+    boolean String5(String s1, String s2) {
+        return s1.equalsIgnoreCase(s2) && !s1.equals(s2);
+    }
 }
