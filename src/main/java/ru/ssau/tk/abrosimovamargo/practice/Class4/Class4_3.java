20a1fab45aa51f1e3ae1df5ff84bb67f98c82b45
abrosimovamargo <abrosimovamargo@yandex.ru>
2020-12-28 18:12:44 +0300

    Задание 4.3

---
+++ src/main/java/ru/ssau/tk/abrosimovamargo/practice/Class4/Class4_3.java
@@ -0,0 +1,21 @@
+package ru.ssau.tk.abrosimovamargo.practice.Class4;
+
+public class Class4_3 {
+    public static String[] getSymbolOnIndex(String[] str, int n) {
+        String[] newStr = new String[str.length];
+        for (String s : str) {
+            if (n > s.length()) {
+                throw new StringIndexOutOfBoundsException();
+            }
+        }
+        for (String s : str) {
+            if (s == null) {
+                throw new NullPointerException();
+            }
+        }
+        for (int i = 0; i < str.length; i++) {
+            newStr[i] = str[i].substring(n, n + 1);
+        }
+        return newStr;
+    }
+}
