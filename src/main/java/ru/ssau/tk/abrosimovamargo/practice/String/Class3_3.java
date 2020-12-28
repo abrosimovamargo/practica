13cda9449ade1b5dc9285e8a5c61d3054fd32822
abrosimovamargo <abrosimovamargo@yandex.ru>
2020-12-28 17:43:02 +0300

    Задание 3.3 ==сравнивает ссылки, а equals() значения

--- src/main/java/ru/ssau/tk/abrosimovamargo/practice/String/Class3_3.java
+++ src/main/java/ru/ssau/tk/abrosimovamargo/practice/String/Class3_3.java
@@ -1,5 +1,12 @@
 package ru.ssau.tk.abrosimovamargo.practice.String;
 
 public class Class3_3 {
+    void sameStrings() {
+        String s1 = "A";
+        String s2 = new String(s1);
 
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+
+    }
 }
