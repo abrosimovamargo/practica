8d16a14941698b42eeb3bc865bc69eb6ae09261d
abrosimovamargo <abrosimovamargo@yandex.ru>
2020-12-28 18:10:02 +0300

    Задание 4.2

---
+++ src/test/java/ru/ssau/tk/abrosimovamargo/practice/Class4/Class4_2Test.java
@@ -0,0 +1,23 @@
+package ru.ssau.tk.abrosimovamargo.practice.Class4;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class Class4_2Test {
+
+    @Test
+    public void testGetSymbolOnIndex() {
+        String[] str = {"I", "believe", "i", "can", "fly"};
+        assertEquals(Class4_2.getSymbolOnIndex(str, 0), new String[]{"I", "b", "i", "c", "f"});
+        Assert.assertThrows(StringIndexOutOfBoundsException.class,
+                () -> assertEquals(Class4_2.getSymbolOnIndex(str, 1),
+                        new String[]{"", "e", "", "a", "l"}));
+        Assert.assertThrows(NullPointerException.class,
+                () -> assertNull(Class4_2.getSymbolOnIndex(null, 0)));
+        String[] str1 = {null, "null"};
+        Assert.assertThrows(NullPointerException.class,
+                () -> assertNull(Class4_2.getSymbolOnIndex(str1, 0)));
+    }
+}
