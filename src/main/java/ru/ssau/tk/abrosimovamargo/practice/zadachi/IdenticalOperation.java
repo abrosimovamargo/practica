a5e8246eaa98b06b175ba8fceba09a840a222eff
abrosimovamargo <abrosimovamargo@yandex.ru>
2020-12-29 10:10:21 +0300

    Задание 1.15

---
+++ src/main/java/ru/ssau/tk/abrosimovamargo/practice/zadachi/IdenticalOperation.java
@@ -0,0 +1,8 @@
+package ru.ssau.tk.abrosimovamargo.practice.zadachi;
+
+public class IdenticalOperation extends Operation {
+    @Override
+    double apply(double number) {
+        return number;
+    }
+}
